0d14a480644e45513b5952b3060f859c32cd49a8
SirHertzAlot <Dukjoseph@gmail.com>
2023-08-03 17:51:58 -0400

    First commit, MVP.

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MessageBox from './MessageBox';
+
+const App = () => <MessageBox nameVal="Joe Blow" natureVal="Tech Support" queryVal="Send help immediately!" />;
+
+export default App;
